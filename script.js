f2643475d5f4377e386d47cf58e110e9966675be
Mohseen365 <mohammedmohseen365@gmail.com>
2023-02-25 15:31:55 +0530

    Ticket

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const addBtn = document.querySelector('.add-btn');
-const modalContainer = document.querySelector('.modal-cont')
-const textareaContainer = document.querySelector('.textarea-cont');
+// import { v4 as uuidv4, v6 as uuidv6 } from 'uuid';
+// const { 
+//   v1: uuidv1,
+//   v4: uuidv4,
+// } = require('uuid');
 
+var uid = new ShortUniqueId(); //we have not installed npm so we use cdn for browser in index.html
+
+const addBtn = document.querySelector('.add-btn');
+const modalContainer = document.querySelector('.modal-container')
+const textareaContainer = document.querySelector('.textarea-container');
+const allPriorityColors = document.querySelectorAll('.priority-color');
+const mainContainer = document.querySelector('.main-container');
+
+const colors = ["lightpink", "lightgreen", "lightblue", "black"];
+let modalPriorityColor = colors[colors.length - 1];
 var isModalPresent = false;
 
-addBtn.addEventListener('click', showModalContainer);
-textareaContainer.addEventListener('keydown', createTask)
-
-function showModalContainer( event ) {
-  console.log('event : ', event);
+addBtn.addEventListener('click', function ( ) {
   if (isModalPresent == false) {
     modalContainer.style.display = 'flex';
     isModalPresent = true;
@@ -16,9 +24,9 @@ function showModalContainer( event ) {
     modalContainer.style.display = 'none';
     isModalPresent = false;
   }
-}
+});
 
-function createTask ( event ) {
+textareaContainer.addEventListener('keydown', function ( event ) {
   if (event.key == 'Shift') {
     console.log('textareaContainer : ', textareaContainer.value);
     createTicket();
@@ -26,4 +34,36 @@ function createTask ( event ) {
     isModalPresent = false;
     textareaContainer.value = '';
   }
+});
+allPriorityColors.forEach(colorElement => {
+  colorElement.addEventListener('click', function () {
+    allPriorityColors.forEach(color => {
+      color.classList.remove("active");
+    });
+    
+    colorElement.classList.add("active"); //classlist is used to add custom class in elements
+    modalPriorityColor = colorElement.classList[0];
+  });
+
+});
+
+
+function createTicket() {
+  var value = textareaContainer.value;
+  var color = modalPriorityColor;
+  var id = uid();
+
+  var ticketContainer = document.createElement('div');
+  ticketContainer.setAttribute('class', 'ticket-container');
+  ticketContainer.innerHTML = `
+    <div class = "ticket-color ${color}"></div>
+    <div class = "ticket-id">#${id}</div>
+    <div class = "task-area">${value}</div>
+    <div class = "ticket-lock">
+      <i class="fa-solid fa-lock"></i>
+    </div>
+  `;
+  mainContainer.appendChild(ticketContainer);
 }
+
+
